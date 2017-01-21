ec869c13bffe6926cf12c09665255ae563a97720
longlk-PC\longlk <darknesszx@gmail.com>
2017-01-21 12:51:35 +0700

    Hook smooth moving

--- js/FishingHook.js
+++ js/FishingHook.js
@@ -5,12 +5,11 @@ class FishingHook extends Phaser.Sprite{
     Fishing.game.add.existing(this);
     this.anchor.setTo(0.5,-4);
     this.movingRight = true;
+    this.angle = 60;
     //this.addChild(this.hook2 = Fishing.game.add.sprite(0,3 * this.height,'hook'));
     Fishing.hookTween = Fishing.game.add.tween(this);
-    Fishing.hookTween.to({ angle: 60 }, 1500, Phaser.Easing.Circular.Out);
-    Fishing.hookTween.to({ angle: 0 }, 3000, Phaser.Easing.Circular.In);
-    Fishing.hookTween.to({ angle: -60 }, 1500, Phaser.Easing.Circular.Out);
-    Fishing.hookTween.to({ angle: 0 }, 3000, Phaser.Easing.Circular.In);
+    Fishing.hookTween.to({ angle: -60 }, 3000, Phaser.Easing.Cubic.InOut);
+    Fishing.hookTween.to({ angle: 60 }, 3000, Phaser.Easing.Cubic.InOut);
     Fishing.hookTween.loop(true);
 
     Fishing.hookTween.start();
